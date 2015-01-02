19f9af1514cdff611bfb9b143ef67c84e08cb574
Chase Southard <chase.southard@gmail.com>
2015-01-01 23:56:01 -0500

    maybe this zaps the tabs

--- server.go
+++ server.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"github.com/codegangsta/negroni"
-	"github.com/gorilla/mux"
-	"net/http"
+  "fmt"
+  "github.com/codegangsta/negroni"
+  "github.com/gorilla/mux"
+  "net/http"
 )
 
 func main() {
-	// classic provides Recovery, Logging, Static default middleware
-	n := negroni.Classic()
+  // classic provides Recovery, Logging, Static default middleware
+  n := negroni.Classic()
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
+  router := mux.NewRouter()
+  router.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w, "Hello World!")
+  })
 
-	// router goes last
-	n.UseHandler(router)
-	n.Run(":3000")
+  // router goes last
+  n.UseHandler(router)
+  n.Run(":3000")
 }
