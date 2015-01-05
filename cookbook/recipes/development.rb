529a34a40719ce9d243e8fc2ccdbf123d9a595df
Chase Southard <chase.southard@gmail.com>
2015-01-05 15:58:13 -0500

    seems to fix go

--- cookbook/recipes/development.rb
+++ cookbook/recipes/development.rb
@@ -1,15 +1,21 @@
 # Cookbook Name:: streakers
 # Recipe:: development
 
-include_recipe 'streakers::default'
+node.set['go']['owner'] = 'vagrant'
 
 magic_shell_environment 'GOROOT' do
   value '/usr/local/go'
 end
 
 magic_shell_environment 'GOPATH' do
-  value '/home/vagrant/streakers'
+  value '/home/vagrant'
 end
+
+magic_shell_environment 'GOBIN' do
+  value '$GOPATH/bin'
+end
+
+include_recipe 'streakers::default'
 
 # TODO(chase): install goop: go get github.com/nitrous-io/goop
 # TODO(chase): run `goop install` in ~/streakers
