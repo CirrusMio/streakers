b3be13c3c62057e58b23d9d694f8c6dbfb6d5248
Chase Southard <chase.southard@gmail.com>
2015-01-05 14:55:52 -0500

    convergence

--- cookbook/recipes/default.rb
+++ cookbook/recipes/default.rb
@@ -5,3 +5,4 @@
 include_recipe 'annoyances'
 include_recipe 'build-essential'
 include_recipe 'git'
+include_recipe 'golang'
--- cookbook/recipes/development.rb
+++ cookbook/recipes/development.rb
@@ -2,3 +2,14 @@
 # Recipe:: development
 
 include_recipe 'streakers::default'
+
+magic_shell_environment 'GOROOT' do
+  value '/usr/local/go'
+end
+
+magic_shell_environment 'GOPATH' do
+  value '/home/vagrant/streakers'
+end
+
+# TODO(chase): install goop: go get github.com/nitrous-io/goop
+# TODO(chase): run `goop install` in ~/streakers
