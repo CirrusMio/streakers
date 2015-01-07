758dc8bfc6a0280f873ef8d0cfd18d8cdfa552fe
Chase Southard <chase.southard@gmail.com>
2015-01-06 23:44:43 -0500

    should be easier than this

--- cookbook/recipes/default.rb
+++ cookbook/recipes/default.rb
@@ -7,3 +7,7 @@ include_recipe 'build-essential'
 include_recipe 'git'
 include_recipe 'golang'
 include_recipe 'golang::packages'
+
+node.set['postgresql']['password']['postgres'] = 'password'
+include_recipe 'postgresql::server'
+include_recipe 'postgresql::client'
