b6b1ad3c2b41d48386e8a9cd25bfdca4e5fae493
Chase Southard <chase.southard@gmail.com>
2015-01-09 23:42:44 -0500

    add gorm; add db connection, update struct

--- server.go
+++ server.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+  _ "database/sql"
   "encoding/json"
   "fmt"
   "github.com/codegangsta/negroni"
   "github.com/gorilla/mux"
+  "github.com/jinzhu/gorm"
   "github.com/joho/godotenv"
+  _ "github.com/lib/pq"
   "log"
   "net/http"
   "os"
 )
 
 type Hacker struct {
-  Name    string
-  Hobbies []string
-  Today   bool
+  Id    int64
+  Name  string
+  Today bool
 }
 
 func main() {
@@ -24,8 +27,17 @@ func main() {
   }
 
   db_user := os.Getenv("DATABASE_USER")
+  db_pass := os.Getenv("DATABASE_PASS")
+  db_host := os.Getenv("DATABASE_HOST")
+  db_name := os.Getenv("DATABASE_NAME")
   logger := log.New(os.Stdout, "", log.Ldate|log.Lmicroseconds|log.Lshortfile)
   logger.Println("got db_user: ", db_user)
+
+  db, err := gorm.Open("postgres", fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=disable", db_user, db_name, db_pass, db_host))
+  logger.Println("db_err: ", err)
+  db.DB()
+  db.LogMode(true)
+  db.AutoMigrate(Hacker{})
 
   // classic provides Recovery, Logging, Static default middleware
   n := negroni.Classic()
@@ -46,8 +58,9 @@ func main() {
 // learned from: http://www.alexedwards.net/blog/golang-response-snippets#json
 func hacker_handler(w http.ResponseWriter, r *http.Request) {
   params := mux.Vars(r) // from the request
-  my_little_json := Hacker{params["hacker"], []string{"music", "programming"},
-                           today{params["hacker"]}}
+  // need to figure out how to create record from params/Vars
+  // return/display JSON dump of saved object
+  my_little_json := Hacker{1, params["hacker"], today("hacker")}
 
   js, err := json.Marshal(my_little_json)
   if err != nil {
@@ -59,6 +72,6 @@ func hacker_handler(w http.ResponseWriter, r *http.Request) {
   w.Write(js)
 }
 
-func today(h string) bool{
+func today(h string) bool {
   return false
 }
